5d8f09731494b1ac1480daa1d8c0a96ab9d1fb19
Jesus <Jesus@DESKTOP-CEJFNK5>
2020-11-18 18:47:17 +0100

    Proyecto Original

---
+++ JesusAdivino/src/Aciertos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/*El proyecto plantea un juego: adivinar el número que genera de manera aleatoria el programa.
+Primero se genera el número aleatorio, a continuación se pide al usuario que introduzca un número entero.
+Si el valor es menor o mayor del número generado, se proporciona un texto orientativo y continua esperando un nuevo número. 
+En caso de acierto se anuncia el éxito, y la aplicación finaliza.*/
+public class Aciertos {
+
+    public static void main(String[] args) {
+        int n = 0,cont = 0;
+        int x = (int) (500 *Math.random());
+        //System.out.print(x);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();
+    }
+}
